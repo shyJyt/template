c3b9fef5236e91d1aa450f42107f226a7239395f
shyJyt <2873704761@qq.com>
2024-04-12 10:13:43 +0800

    init

---
+++ base.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <stack>
+#include <climits>
+#include <bitset>
+#define mt make_tuple
+#define ll long long
+#define ull unsigned long long
+using namespace std;
+typedef pair<int, int> pii;
+typedef pair<int, ll> pil;
+typedef pair<ll, ll> pll;
+const int MAXN = 2e5 + 10;
+int n;
+
+void solve() {
+	cin >> n;
+	
+	return ;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int t;
+//	cin >> t;
+	t = 1;
+	while(t--) {
+		solve();
+	}
+	return 0;
+}
